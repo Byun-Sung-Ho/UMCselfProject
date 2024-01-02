98eb7ec0856e2cbf2f79812d62ec96a69f850157
201910806ByunSungHo <bsh4237@gmail.com>
2024-01-02 23:23:36 +0900

    first commit

---
+++ UMCselfPractice/src/main/java/com/example/umcselfpractice/UMCselfPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.umcselfpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UMCselfPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UMCselfPracticeApplication.class, args);
+    }
+
+}
---
+++ UMCselfPractice/src/test/java/com/example/umcselfpractice/UMCselfPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umcselfpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UMCselfPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
